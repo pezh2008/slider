f94e0b063cf2f814edcbed1caff1ec35bb40a3be
Pier Bomarshegit config --global user.email pezh2008@gmail.com <84318712+pezh2008@users.noreply.github.com>
2022-07-18 22:20:33 +0300

    test-js

---
+++ js/script.js
@@ -0,0 +1,85 @@
+let city = document.querySelector(".brest");
+let user = document.querySelector(".dima");
+let age = document.querySelector(".age");
+let ag = [24,25,26, "sveta"]
+
+console.log( "привет", ag[3] );
+
+// for(let i=0; i<ag[2]; i++){
+
+//   console.log(ag[1]+ag[2]+ag[3])
+// }
+
+const sveta = {user, ag, city};
+console.log(sveta);
+console.log(ag[2])
+function test ()
+
+let one = 1;
+let two = 2;
+let summ = one + two;
+
+one + two
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const slides = Array.from(document.querySelectorAll("slide"));
+// const slider = document.querySelector(".slider");
+// const buttons = document.querySelectorAll(".butt");
+
+// function getleftright(){
+//   const activeSlide = document.querySelector(".slide active")
+//   const activeIndex = slides.indexOf("activeIndex")
+//   console.log(activeIndex);
+
+// }
+// getleftright();
+
+// let user = "dima";
+// let age = 31;
+// let message = "Hello"
+// for (let i = 0; i<user; i++) {
+  
+//   console.log (age)
+// }
+
+// let zero = 0;
+// while (zero < 5) {
+//   console.log(zero);
+//   zero++;
+// }
+// let left = document.querySelectorAll("btn-left butt");
+// let right = document.querySelectorAll("btn-right butt");
+// for(i = 0; i<fifti; i++)
